06f7b2bbf0e38244a12765b47bf8a783c43eb9b9
OskierP <pakooskar@gmail.com>
2022-11-21 14:35:57 +0100

    ypu can navigate now

---
+++ fitness-app-group7/src/GetUserPage.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+
+import { Link } from 'react-router-dom';
+
+export function GetUsersPage() {
+
+  return (
+    <div className='App'>
+
+        <Link to='/'>Home</Link>
+        <div> dddd</div>
+
+    </div>
+
+
+
+ 
+  );
+}
+
+// export default GetUsersPage;
